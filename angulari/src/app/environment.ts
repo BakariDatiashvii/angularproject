8b672120b686f0673c870406a41dd98f8b493617
BakariDatiashvii <baqari.datiashvili@gmail.com>
2024-10-15 17:18:12 +0400

    stores

--- angulari/src/app/environment.ts
+++ angulari/src/app/environment.ts
@@ -1,22 +1,19 @@
- export const environment = {
+const baseUrl = "http://localhost:5133/api";
+
+export const environment = {
     production: true,
-    loginUrl: 'http://localhost:5133/api/Employee/login',  
-    addstoreapiUrl: 'http://localhost:5133/api/Employee' ,
-    registermanagerandoperatorUrl:  'http://localhost:5133/api/Employee/register-managerandoperator' ,    
-    getmanagerstoreproductUrl: 'http://localhost:5133/api/Employee/managergetall',    
-    getoperatortproductUrl: 'http://localhost:5133/api/Employee/operatorgetall',
-    registeradminbycompanyUrl:  'http://localhost:5133/api/Employee/create',
-    updateusermanagerandoperatorUrl:  'http://localhost:5133/api/Employee/update-user',
-    getuserbyidUrl: 'http://localhost:5133/api/Employee/get-user-by-id',
-    getallstoresUrl: 'http://localhost:5133/api/Employee/get-all-stores',
-    
-    addProductUrl: 'http://localhost:5133/api/Employee/add-product-opretor',
+    loginUrl: `${baseUrl}/Employee/login`,
+    addstoreapiUrl: `${baseUrl}/Employee`,
+    registermanagerandoperatorUrl: `${baseUrl}/Employee/register-managerandoperator`,
+    getmanagerstoreproductUrl: `${baseUrl}/Employee/managergetall`,
+    getoperatortproductUrl: `${baseUrl}/Employee/operatorgetall`,
+    registeradminbycompanyUrl: `${baseUrl}/Employee/create`,
+    updateusermanagerandoperatorUrl: `${baseUrl}/Employee/update-user`,
+    getuserbyidUrl: `${baseUrl}/Employee/get-user-by-id`,
+    getallstoresUrl: `${baseUrl}/Employee/get-all-stores`,
+    addProductUrl: `${baseUrl}/Employee/add-product-opretor`,
+    updateproductsemosUrl: `${baseUrl}/Employee/update-product`,
+    updateproductsellUrl: `${baseUrl}/Employee/update-productSell`
+};
 
-    updateproductsemosUrl: 'http://localhost:5133/api/Employee/update-product',
-
-    updateproductsellUrl: 'http://localhost:5133/api/Employee/update-productSell',
-
-    
-    
-  };
   
